a40133c5c50d6582daa40bbddb6543228983d693
Mauricio Ortega <maudan1098@gmail.com>
2024-01-22 12:49:53 -0600

    fix potentiometer example

--- examples/Potentiometer_Example/PotentiometerExample.ino
+++ examples/Potentiometer_Example/PotentiometerExample.ino
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+
+//Potentiometer library
 #include <Potentiometer.h>
 
 void setup() 
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
 }
 
 void loop() 
